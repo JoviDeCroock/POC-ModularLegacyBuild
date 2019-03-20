6726be1a3fdd3b18b6148c3e6281da0444e0acfb
Jovi De Croock <decroockjovi@gmail.com>
2019-03-20 17:05:35 +0100

    chore: update bablerc to new corejs version

--- .babelrc.js
+++ .babelrc.js
@@ -1,47 +1,52 @@
 const plugins = [
   "@babel/plugin-syntax-dynamic-import",
-  "@babel/plugin-proposal-export-default-from"
-]
+  "@babel/plugin-proposal-export-default-from",
+  [
+    require.resolve('@babel/plugin-transform-react-jsx'),
+  ],
+];
+
+const defaultEnv = {
+  debug: false,
+  exclude: ["@babel/plugin-transform-typeof-symbol"],
+  modules: false,
+  loose: true,
+}
 
 module.exports = {
   env: {
     legacy: {
       presets: [
-        ["@babel/preset-react", { useBuiltIns: true }],
         [
           "@babel/preset-env", {
-            modules: false,
-            targets: ['ie 9'],
-            loose: true,
+            ...defaultEnv,
+            corejs: 3,
+            targets: {
+              browsers: ["last 2 versions", "ie >= 11"]
+            },
+            useBuiltIns: 'entry',
           }
         ]
       ],
       plugins: [
         ...plugins,
         ["@babel/plugin-transform-runtime", {
-          corejs: 2
+          corejs: 3,
         }]
       ],
     },
     modern: {
       presets: [
-        ["@babel/preset-react", { useBuiltIns: true }],
         [
           "@babel/preset-env", {
-            modules: false,
+            ...defaultEnv,
+            corejs: 3,
             targets: { esmodules: true },
-            loose: true,
             useBuiltIns: 'usage',
           }
         ]
       ],
-      plugins: [
-        ...plugins,
-        ["@babel/plugin-transform-runtime", {
-          corejs: 2,
-          useESModules: true,
-        }]
-      ],
+      plugins,
     }
   }
 };
